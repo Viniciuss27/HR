f8ad9ee3e78ea3446f47ecc508a0bc2175069b95
vinicius <viniciusdejjeuspessoal@gmail.com>
2026-06-21 13:52:27 -0300

    Exceptions

--- Worker/src/main/java/vinix/services/exceptions/ResourceNotFoundException.java
+++ Worker/src/main/java/vinix/services/exceptions/ResourceNotFoundException.java
@@ -4,6 +4,6 @@ public class ResourceNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public ResourceNotFoundException(Object id) {
-		super("Recurso não encontrado. Id: " + id);
+		super("Recurso não encontrado Id: " + id);
 	}
 }
