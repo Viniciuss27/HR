af98e4305a6e411a8795118cb79a74f1e4d01e59
vinicius <viniciusdejjeuspessoal@gmail.com>
2026-06-24 09:02:26 -0300

    adicionando lombok e melhor versão do springboot

--- User/src/main/java/vinix/entities/Role.java
+++ User/src/main/java/vinix/entities/Role.java
@@ -1,7 +1,6 @@
 package vinix.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,8 +8,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "tb_role")
 public class Role implements Serializable{
@@ -22,45 +26,4 @@ public class Role implements Serializable{
 	
 	@Column(name = "role_name")
 	private String roleName;
-	
-	public Role() {}
-
-	public Role(Long id, String roleName) {
-		super();
-		this.id = id;
-		this.roleName = roleName;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getRoleName() {
-		return roleName;
-	}
-
-	public void setRoleName(String roleName) {
-		this.roleName = roleName;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Role other = (Role) obj;
-		return Objects.equals(id, other.id);
-	}
 }
